1306b2b47e1982cb50396bfa513e5cd7aa3e50ac
Nahid51 <hasannahidnahid@gmail.com>
2022-01-28 20:46:56 +0600

    added blogs' delete and update system

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     const database = client.db("HolidaysHype");
     const blogCollection = database.collection("services");
+    const reviewCollection = database.collection("reviews");
     const usersCollection = database.collection("users");
     console.log('connected successfully');
 
     // add service data to database
     app.post('/addBlogs', async (req, res) => {
         const doc = req.body;
-        console.log(doc);
         const result = await blogCollection.insertOne(doc);
         res.send(result);
     })
+
     // get services from database
     app.get('/blogs', async (req, res) => {
-        console.log(req.query);
         const cursor = blogCollection.find({});
         const page = req.query.page;
         const size = parseInt(req.query.size);
@@ -45,6 +45,62 @@ client.connect(err => {
         });
     })
 
+    // add review data to database
+    app.post('/addReview', async (req, res) => {
+        const doc = req.body;
+        const result = await reviewCollection.insertOne(doc);
+        res.send(result);
+    })
+
+    // get review data from database
+    app.get('/reviews', async (req, res) => {
+        const cursor = reviewCollection.find({});
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    //get all blogs
+    app.get('/allBlogs', async (req, res) => {
+        const allblogs = blogCollection.find({});
+        const result = await allblogs.toArray();
+        res.send(result);
+    })
+
+    //delete a blog
+    app.delete('/deleteBlog/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await blogCollection.deleteOne(query);
+        res.send(result);
+    })
+
+    // update a blog
+    app.get('/updateBlog/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await blogCollection.findOne(query);
+        res.send(result);
+    })
+    app.put('/updateBlog/:id', async (req, res) => {
+        const id = req.params.id;
+        const updatePd = req.body;
+        console.log(id, updatePd);
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+                location: updatePd.location,
+                photo: updatePd.photo,
+                price: updatePd.price,
+                description: updatePd.description,
+                rating: updatePd.rating,
+                date: updatePd.date,
+            },
+        };
+        const result = await blogCollection.updateOne(filter, updateDoc, options);
+        res.send(result);
+    })
+
     // add new users (by registration) to database
     app.post('/users', async (req, res) => {
         const user = req.body;
@@ -60,6 +116,7 @@ client.connect(err => {
         const result = await usersCollection.updateOne(filter, updateDoc, options);
         res.send(result);
     })
+
     // make admin 
     app.put('/users/admin', async (req, res) => {
         const email = req.body.admin;
@@ -75,6 +132,7 @@ client.connect(err => {
         }
         else { res.status(403).send({ message: 'Access Denied' }) }
     })
+
     // get admin from database
     app.get('/users/:email', async (req, res) => {
         const email = req.params.email;
